f5da2492f68116581754ed3bb4c2327ac0584eb1
denzow <denzow@gmail.com>
2018-08-27 00:20:48 +0900

    [add] sanicで少しキャッシュを改善
    ubuntu@ip-172-31-23-113:~$ ./benchmark --ip 18.179.197.192 --workload 30
    2018/08/26 15:18:51 Start GET /initialize
    2018/08/26 15:18:51 期日前投票を開始します
    2018/08/26 15:18:52 期日前投票が終了しました
    2018/08/26 15:18:52 投票を開始します  Workload: 30
    2018/08/26 15:19:37 投票が終了しました
    2018/08/26 15:19:37 投票者が結果を確認しています
    2018/08/26 15:19:52 投票者の感心がなくなりました
    2018/08/26 15:19:52 {"score": 145595, "success": 125147, "failure": 0}
    
    Worker=1のほうがスコアが伸びるのでRedis切り離せるかも。

--- python/sanic_app.py
+++ python/sanic_app.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import pathlib
 from urllib.parse import unquote_plus
+import asyncio
 
 import constants
 import jinja2
@@ -27,7 +28,18 @@ jinja2_sanic.setup(
     loader=jinja2.FileSystemLoader('./templates', encoding='utf8')
 )
 render_template = jinja2_sanic.render_template
+render_string = jinja2_sanic.render_string
 
+
+"""
+    text = render_string(template_name, request, context, app_key=app_key)
+    content_type = "text/html; charset={encoding}".format(encoding=encoding)
+
+    return HTTPResponse(
+        text, status=status, headers=headers,
+        content_type=content_type
+    )
+"""
 
 @app.listener('before_server_start')
 async def aio_redis_configure(_app, loop):
@@ -92,36 +104,44 @@ def db_initialize():
 
 @app.route('/')
 async def get_index(request):
-    candidates = []
-    election_results = await get_election_results()
-    # 上位10人と最下位のみ表示
-    candidates += election_results[:10]
-    candidates.append(election_results[-1])
+    html = await get_index_cache()
+    if not html:
+        candidates = []
+        election_results = await get_election_results()
+        # 上位10人と最下位のみ表示
+        candidates += election_results[:10]
+        candidates.append(election_results[-1])
 
-    parties_name = get_all_party_name()
-    parties = {}
-    for name in parties_name:
-        parties[name] = 0
-    for r in election_results:
-        parties[r['political_party']] += r['count'] or 0
-    parties = sorted(parties.items(), key=lambda x: x[1], reverse=True)
+        parties_name = get_all_party_name()
+        parties = {}
+        for name in parties_name:
+            parties[name] = 0
+        for r in election_results:
+            parties[r['political_party']] += r['count'] or 0
+        parties = sorted(parties.items(), key=lambda x: x[1], reverse=True)
 
-    sex_ratio = {'men': 0, 'women': 0}
-    for r in election_results:
-        if r['sex'] == '男':
-            sex_ratio['men'] += r['count'] or 0
-        elif r['sex'] == '女':
-            sex_ratio['women'] += r['count'] or 0
-    html = render_template(
-        'index.html',
-        request,
-        context=dict(
-            candidates=candidates,
-            parties=parties,
-            sex_ratio=sex_ratio
+        sex_ratio = {'men': 0, 'women': 0}
+        for r in election_results:
+            if r['sex'] == '男':
+                sex_ratio['men'] += r['count'] or 0
+            elif r['sex'] == '女':
+                sex_ratio['women'] += r['count'] or 0
+        html = render_string(
+            'index.html',
+            request,
+            context=dict(
+                candidates=candidates,
+                parties=parties,
+                sex_ratio=sex_ratio
+            )
         )
+        await set_index_cache(html)
+
+    return HTTPResponse(
+        html,
+        status=200,
+        content_type="text/html; charset=utf-8",
     )
-    return html
 
 
 @app.route('/candidates/<candidate_id:int>')
@@ -129,16 +149,24 @@ async def get_candidate(request, candidate_id):
     candidate = get_candidate_by_id(candidate_id)
     if not candidate:
         return response.redirect('/')
-    votes = await get_vote_count_cache_by_candidate_id(candidate_id)
-    keywords = await get_voice_of_supporter_by_id(candidate_id)
-    return render_template(
-        'candidate.html',
-        request,
-        context=dict(
-            candidate=candidate,
-            votes=votes,
-            keywords=keywords,
+    html = await get_candidate_cache(candidate_id)
+    if not html:
+        votes = await get_vote_count_cache_by_candidate_id(candidate_id)
+        keywords = await get_voice_of_supporter_by_id(candidate_id)
+        html = render_string(
+            'candidate.html',
+            request,
+            context=dict(
+                candidate=candidate,
+                votes=votes,
+                keywords=keywords,
+            )
         )
+        await set_candidate_cache(candidate_id, html)
+    return HTTPResponse(
+        html,
+        status=200,
+        content_type="text/html; charset=utf-8",
     )
 
 
@@ -146,22 +174,31 @@ async def get_candidate(request, candidate_id):
 async def get_political_party(request, name):
     votes = 0
     name = constants.QUOTED_PARTY_MAP.get(name)
-    for r in await get_election_results():
-        if r['political_party'] == name:
-            votes += r['count'] or 0
-#    cur.execute('SELECT * FROM candidates WHERE political_party = "{}"'.format(name))
-    candidate_ids = constants.PARTY_MASTER.get(name)
-    candidates = [get_candidate_by_id(candidate_id) for candidate_id in candidate_ids]
-    keywords = await get_voice_of_supporter(candidate_ids)
-    return render_template(
-        'political_party.html',
-        request,
-        context=dict(
-            political_party=name,
-            votes=votes,
-            candidates=candidates,
-            keywords=keywords
+    html = await get_party_cache(name)
+    if not html:
+        for r in await get_election_results():
+            if r['political_party'] == name:
+                votes += r['count'] or 0
+    #    cur.execute('SELECT * FROM candidates WHERE political_party = "{}"'.format(name))
+        candidate_ids = constants.PARTY_MASTER.get(name)
+        candidates = [get_candidate_by_id(candidate_id) for candidate_id in candidate_ids]
+        keywords = await get_voice_of_supporter(candidate_ids)
+        html = render_string(
+            'political_party.html',
+            request,
+            context=dict(
+                political_party=name,
+                votes=votes,
+                candidates=candidates,
+                keywords=keywords
+            )
         )
+        await set_party_cache(name, html)
+
+    return HTTPResponse(
+        html,
+        status=200,
+        content_type="text/html; charset=utf-8",
     )
 
 
@@ -198,10 +235,15 @@ async def post_vote(request):
         return text(constants.VOTE_FAIL5_HTML)
 
     vote_count = int(form_base['vote_count'])
-    await set_vote_count_cache_by_candidate_id(candidate_id, vote_count)
-    await set_vote_keyword_count_cache_by_candidate_id(candidate_id, form_base['keyword'],  vote_count)
-    await set_voted_count_cache_by_user_id(user_id, vote_count)
-
+    party_name = constants.CANDIDATES_MASTER.get(candidate_id).get('political_party')
+    asyncio.gather(
+        set_vote_count_cache_by_candidate_id(candidate_id, vote_count),
+        set_vote_keyword_count_cache_by_candidate_id(candidate_id, form_base['keyword'], vote_count),
+        set_voted_count_cache_by_user_id(user_id, vote_count),
+        clear_page_cache(),
+        clear_party_cache(party_name),
+        clear_candidate_cache(candidate_id),
+    )
     return text(constants.VOTE_SUCCESS_HTML)
 
 
@@ -216,6 +258,13 @@ def myint(base, defalut=0):
     if base:
         return int(base)
     return defalut
+
+def mydecode(base, defalut=None):
+    if base:
+        return base.decode('utf-8')
+    return defalut
+
+
 
 
 def get_candidate_id_by_name(name):
@@ -273,10 +322,48 @@ async def get_user_cache(mynumber, name, address):
     return None
 
 
+async def set_index_cache(html):
+    return await app.redis1.set('index_html', html)
+
+
+async def get_index_cache():
+    return mydecode(await app.redis1.get('index_html'))
+
+
+
+async def clear_page_cache():
+    tasks = [app.redis1.delete('index_html')]
+    await asyncio.gather(*tasks)
+
+
+async def set_party_cache(name, html):
+    await app.redis1.set('party_{}'.format(name), html)
+
+
+async def get_party_cache(name):
+    return mydecode(await app.redis1.get('party_{}'.format(name)))
+
+
+async def clear_party_cache(candidate_id):
+    await app.redis1.delete('candidate_{}'.format(candidate_id))
+
+
+async def set_candidate_cache(candidate_id, html):
+    await app.redis1.set('candidate_{}'.format(candidate_id), html)
+
+
+async def get_candidate_cache(candidate_id):
+    return mydecode(await app.redis1.get('candidate_{}'.format(candidate_id)))
+
+
+async def clear_candidate_cache(candidate_id):
+    await app.redis1.delete('candidate_{}'.format(candidate_id))
+
+
 @lru_cache(maxsize=100)
 def unquote_cached(keyword):
     return unquote_plus(keyword)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, workers=8, access_log=False)
+    app.run(host="0.0.0.0", port=8080, workers=1, access_log=False)
