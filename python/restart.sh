faf5614af4294e5e3c9209c196477de5be75fb42
denzow <denzow@gmail.com>
2018-08-26 21:46:10 +0900

    [clean] 不要なコードを整理

--- python/restart.sh
+++ python/restart.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -x
 export BASE_DIR=/home/ishocon/webapp/python
-echo 'mysql'
-sudo cp ${BASE_DIR}/etc/mysql/my.cnf  /etc/mysql/my.cnf
-sudo service mysql stop
+#echo 'mysql'
+#sudo cp ${BASE_DIR}/etc/mysql/my.cnf  /etc/mysql/my.cnf
+#sudo service mysql stop
 
 echo 'nginx'
 sudo cp ${BASE_DIR}/etc/nginx/nginx.conf  /etc/nginx/nginx.conf
 sudo service nginx restart
 
 
-echo 'redis'
-sudo cp ${BASE_DIR}/etc/redis/redis.conf  /etc/redis/redis.conf
-sudo service redis restart
+#echo 'redis'
+#sudo cp ${BASE_DIR}/etc/redis/redis.conf  /etc/redis/redis.conf
+#sudo service redis restart
